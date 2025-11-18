70e10fe78ab5b5635fb3ab73dcfcaded48e043d6
vsuminv <leesumin@leesuminui-MacBookAir.local>
2025-11-18 11:15:24 +0900

    feat(hub): 환경셋팅

---
+++ src/main/java/com/early_express/hub_service/HubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.early_express.hub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubServiceApplication.class, args);
+	}
+
+}
